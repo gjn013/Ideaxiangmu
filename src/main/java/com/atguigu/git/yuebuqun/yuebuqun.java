0b564d2c6c9b7969a60ae8f2df2bda002494beef
zhangchen <gjn013@sina.com>
2020-12-15 19:00:34 +0800

    创建测试项目

---
+++ src/main/java/com/atguigu/git/yuebuqun/yuebuqun.java
@@ -0,0 +1,17 @@
+package com.atguigu.git.yuebuqun;
+
+/**
+ * @author 爸爸
+ * @create 2020-12-15 18:36
+ */
+public class yuebuqun {
+    public static void main(String[] args) {
+        System.out.println("独孤九键好");
+        System.out.println("独孤九键好");
+        System.out.println("独孤九键好");
+        System.out.println("独孤九键好");
+        System.out.println("独孤九键好");
+        System.out.println("独孤九键好");
+
+    }
+}
